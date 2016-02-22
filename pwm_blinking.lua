54909e5e4248b02475653296c9a360b396803314
Bunyawat Singchai <bunyawat.s@gmail.com>
2016-02-22 17:49:04 +0700

    simple blink with PWM

--- pwm_blinking.lua
+++ pwm_blinking.lua
@@ -10,28 +10,25 @@ function led(r, g, b)
     pwm.setduty(3, b)
 end
 
-
-
 direc = 1
 i = 0
 function blink() 
     if direc == 1 then
-        if i < 300 then
-            i = i + 20
+        if i < 200 then
+            i = i + 5
         else
             direc = 0
              print("chang direction "..0)
         end
     else
         if i > 0 then
-            i = i - 20
+            i = i - 5
         else
             direc = 1
             print("chang direction "..1)
         end
     end
     led( i, 0, 0 )
-    print( i )
 end
 
-tmr.alarm(0, 500, 1, blink)
+tmr.alarm(0, 50, 1, blink)
