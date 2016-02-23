48039f6c97c87684eee8a33988ad43cba61fa0fc
Bunyawat Singchai <bunyawat.s@gmail.com>
2016-02-23 21:28:25 +0700

    brink 3 colors LED with PWM

--- pwm_blinking.lua
+++ pwm_blinking.lua
@@ -1,20 +1,16 @@
 
-pwm.stop(1)
-pwm.setup(1, 500, 100)
-pwm.start(1)
+LED = 1
 
+pwm.stop(LED)
+pwm.setup(LED, 500, 0)
+pwm.start(LED)
 
-function led(r, g, b)
-    pwm.setduty(1, r)
-    pwm.setduty(2, g)
-    pwm.setduty(3, b)
-end
 
 direc = 1
 i = 0
 function blink() 
     if direc == 1 then
-        if i < 200 then
+        if i < 500 then
             i = i + 5
         else
             direc = 0
@@ -28,7 +24,8 @@ function blink()
             print("chang direction "..1)
         end
     end
-    led( i, 0, 0 )
+    pwm.setduty(LED, i)
+
 end
 
 tmr.alarm(0, 50, 1, blink)
