7deb045adee66dbf8239d236cc925b759204923e
Bunyawat Singchai <bunyawat.s@gmail.com>
2016-03-02 22:15:14 +0700

    specific topic to device id

--- mqtt/test.lua
+++ mqtt/test.lua
@@ -8,5 +8,6 @@ setup.start()
 
 -- dofile("test.lua")
 -- mosquitto_sub -h test.mosquitto.org  -p 1883 -t nodemcu/lua/# -v
--- mosquitto_pub -h 192.168.0.15 -p 1883 -t nodemcu/lua/ -m "led0/on"
+-- mosquitto_pub -h 192.168.1.3 -p 1883 -t nodemcu/lua/10394073 -m "led0/off"
+-- mosquitto_pub -h 192.168.1.3 -p 1883 -t nodemcu/lua/10394073 -m "led0/on"
 -- mosquitto_sub -h 192.168.0.15 -p 1883 -t nodemcu/lua/# -v
